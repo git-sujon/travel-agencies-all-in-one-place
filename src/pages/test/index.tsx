cafa59f37b99b696232b333b1ec872441039c0bd
Moniruzzaman Sujon <git.sujon@gmail.com>
2023-08-24 01:25:00 +0600

    _app.tsx file created for individual layout and a test page created for example

---
+++ src/pages/test/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TestPage = () => {
+    return (
+        <div>
+            this is a test page and it will removed
+        </div>
+    );
+};
+
+export default TestPage;
