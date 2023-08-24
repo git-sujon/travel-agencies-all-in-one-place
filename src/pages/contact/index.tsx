89cccfd9ad4d513b584ec43bdffd8107d046ece2
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-08-24 13:09:32 +0600

    contact info section added on contact page

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import ContactCard from "@/components/contactPage/contactCard/ContactCard";
+import {FaRegEnvelopeOpen } from "react-icons/fa";
+import { BsGeoAlt, BsTelephoneInbound } from "react-icons/bs";
 
 const ContactUsPage = () => {
     return (
-        <div>
-            this contact us page
+        <div className=' '>
+            <div className="max-w-[1200px] mx-auto border min-h-screen">
+                <div className="grid md:grid-cols-3 py-7 md:py-12">
+                    <ContactCard icon={<FaRegEnvelopeOpen className="w-5 h-5 lg:w-8 lg:h-8"></FaRegEnvelopeOpen>} title={"Email:"} value={"travel2023@gmail.com"}></ContactCard>
+                    <ContactCard icon={<BsGeoAlt className="w-5 h-5 lg:w-8 lg:h-8"></BsGeoAlt>} title={"Address:"} value={"New York, USA"}></ContactCard>
+                    <ContactCard icon={<BsTelephoneInbound className="w-5 h-5 lg:w-8 lg:h-8"></BsTelephoneInbound>} title={"Phone:"} value={"+88017XXXXXXXX"}></ContactCard>
+                </div>
+            </div>
         </div>
     );
 };
