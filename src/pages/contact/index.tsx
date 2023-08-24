f37c345769aa44760eff30f76ac7f25baf9934a0
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-08-24 11:49:34 +0600

    route added

---
+++ src/pages/contact/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactUsPage = () => {
+    return (
+        <div>
+            this contact us page
+        </div>
+    );
+};
+
+export default ContactUsPage;
