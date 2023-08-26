ee2c94d7b5aedc654f63d885ed45b8d7f11b980d
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-08-26 15:02:41 +0600

    package card add

--- src/pages/packages/index.tsx
+++ src/pages/packages/index.tsx
@@ -2,6 +2,8 @@ import RootLayout from '@/components/layout/RootLayout';
 import Image from 'next/image';
 import React from 'react';
 import PackagesFiltering from '@/components/UI/packagesFiltering/PackagesFiltering';
+import PackageCard from '@/components/UI/packageCard/PackageCard';
+
 
 const PackagesPage = () => {
     return (
@@ -14,6 +16,11 @@ const PackagesPage = () => {
                 <div className='px-3 lg:px-5 2xl:px-0 h-[70px] md:h-[100px] lg:h-[130px] xl:h-[180px] bg-white shadow-lg absolute bottom-0 translate-y-1/2 container left-1/2 -translate-x-1/2 flex justify-center items-center rounded-md'>
                     <PackagesFiltering></PackagesFiltering>
                 </div>
+            </div>
+            <div className='max-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-x-8 lg:gap-y-10 my-20 lg:mt-44 '>
+                {
+                    [1, 2, 3, 4, 5, 6].map((item, index) => <PackageCard key={index}></PackageCard>)
+                }
             </div>
         </div>
     );
