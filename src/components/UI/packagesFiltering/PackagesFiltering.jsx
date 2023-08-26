228e868b6fd021ea3e1f1a75debd2253d3f8b245
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-08-26 14:26:57 +0600

    packages banner added and filtering

---
+++ src/components/UI/packagesFiltering/PackagesFiltering.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const PackagesFiltering = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  return (
+    <div className="w-full lg:px-5 2xl:px-8 grid gird-cols-3 lg:grid-cols-5 gap-5 lg:gap-8">
+      <div className="flex flex-col gap-2">
+        <label className="text-base lg:text-lg font-medium text-accent">
+          Region
+        </label>
+        {/* <input className='px-3 py-1 bg-slate-100 outline-none rounded-md text-sm' placeholder='Select Region'/> */}
+        <select className="px-3 py-2 bg-slate-100 outline-none rounded-md text-sm ">
+          <option disabled selected>
+            Select Region
+          </option>
+          <option>Homer</option>
+          <option>Marge</option>
+          <option>Bart</option>
+          <option>Lisa</option>
+          <option>Maggie</option>
+        </select>
+      </div>
+      <div className="flex flex-col gap-2">
+        <label className="text-base lg:text-lg font-medium text-accent">
+          Duration
+        </label>
+        {/* <input className='px-3 py-1 bg-slate-100 outline-none rounded-md text-sm' placeholder='Select Region'/> */}
+        <select className="px-3 py-2 bg-slate-100 outline-none rounded-md text-sm ">
+          <option disabled selected>
+            Select Duration
+          </option>
+          <option>1Days 1Night</option>
+          <option>2Days 2Night</option>
+          <option>3Days 2Night</option>
+          <option>5Days 4Night</option>
+          <option>10Days 10Night</option>
+        </select>
+      </div>
+      <div className="flex flex-col gap-2">
+        <label className="text-base lg:text-lg font-medium text-accent">
+          Date
+        </label>
+        {/* <input className='px-3 py-1 bg-slate-100 outline-none rounded-md text-sm' placeholder='Select Region'/> */}
+        <DatePicker
+          className="px-3 py-2 bg-slate-100 outline-none rounded-md text-sm w-full"
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label className="text-base lg:text-lg font-medium text-accent">
+          Guests
+        </label>
+        <input
+          className="px-3 py-2 bg-slate-100 outline-none rounded-md text-sm"
+          placeholder="Number of Guest"
+        />
+      </div>
+      <div className="flex w-full h-full items-center justify-center">
+        <button className="px-3 h-fit py-2 w-full bg-secondary hover:bg-primary duration-300 mt-7 rounded-md">
+          Search
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PackagesFiltering;
--- src/pages/packages/index.tsx
+++ src/pages/packages/index.tsx
@@ -1,66 +1,18 @@
 import RootLayout from '@/components/layout/RootLayout';
 import Image from 'next/image';
 import React from 'react';
+import PackagesFiltering from '@/components/UI/packagesFiltering/PackagesFiltering';
 
 const PackagesPage = () => {
     return (
         <div className='w-full'>
             <div className='relative'>
-                <div className='absolute text-center'>
-                    <h1>Enjoy your days with 72+ cool packages</h1>
+                <div className='absolute text-center w-full h-full pt-[20vh]'>
+                    <h1 className='text-2xl lg:text-5xl font-bold font-serif text-white max-w-[600px] mx-auto'>Enjoy your days with 72+ cool packages</h1>
                 </div>
                 <Image src='/images/banner.jpg' alt='hero' width={1920} height={1000} className='w-full h-full' />
                 <div className='px-3 lg:px-5 2xl:px-0 h-[70px] md:h-[100px] lg:h-[130px] xl:h-[180px] bg-white shadow-lg absolute bottom-0 translate-y-1/2 container left-1/2 -translate-x-1/2 flex justify-center items-center rounded-md'>
-                    <div className='w-full lg:px-5 2xl:px-8 grid gird-cols-3 lg:grid-cols-5 gap-5 lg:gap-8'>
-                        <div className='flex flex-col gap-2'>
-                            <label className='text-base lg:text-lg font-medium text-accent'>Region</label>
-                            {/* <input className='px-3 py-1 bg-slate-100 outline-none rounded-md text-sm' placeholder='Select Region'/> */}
-                            <select className="px-3 py-2 bg-slate-100 outline-none rounded-md text-sm ">
-                                <option disabled selected>Pick your favorite Simpson</option>
-                                <option>Homer</option>
-                                <option>Marge</option>
-                                <option>Bart</option>
-                                <option>Lisa</option>
-                                <option>Maggie</option>
-                            </select>
-                        </div>
-                        <div className='flex flex-col gap-2'>
-                            <label className='text-base lg:text-lg font-medium text-accent'>Region</label>
-                            {/* <input className='px-3 py-1 bg-slate-100 outline-none rounded-md text-sm' placeholder='Select Region'/> */}
-                            <select className="px-3 py-2 bg-slate-100 outline-none rounded-md text-sm ">
-                                <option disabled selected>Pick your favorite Simpson</option>
-                                <option>Homer</option>
-                                <option>Marge</option>
-                                <option>Bart</option>
-                                <option>Lisa</option>
-                                <option>Maggie</option>
-                            </select>
-                        </div>
-                        <div className='flex flex-col gap-2'>
-                            <label className='text-base lg:text-lg font-medium text-accent'>Region</label>
-                            {/* <input className='px-3 py-1 bg-slate-100 outline-none rounded-md text-sm' placeholder='Select Region'/> */}
-                            <select className="px-3 py-2 bg-slate-100 outline-none rounded-md text-sm ">
-                                <option disabled selected>Pick your favorite Simpson</option>
-                                <option>Homer</option>
-                                <option>Marge</option>
-                                <option>Bart</option>
-                                <option>Lisa</option>
-                                <option>Maggie</option>
-                            </select>
-                        </div>
-                        <div className='flex flex-col gap-2'>
-                            <label className='text-base lg:text-lg font-medium text-accent'>Region</label>
-                            {/* <input className='px-3 py-1 bg-slate-100 outline-none rounded-md text-sm' placeholder='Select Region'/> */}
-                            <select className="px-3 py-2 bg-slate-100 outline-none rounded-md text-sm ">
-                                <option disabled selected>Pick your favorite Simpson</option>
-                                <option>Homer</option>
-                                <option>Marge</option>
-                                <option>Bart</option>
-                                <option>Lisa</option>
-                                <option>Maggie</option>
-                            </select>
-                        </div>
-                    </div>
+                    <PackagesFiltering></PackagesFiltering>
                 </div>
             </div>
         </div>
